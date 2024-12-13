1a11bb45225fd2aa684e0122023b08ab355b53e7
paddingtonowner <dianapatrice@gmail.com>
2024-12-13 18:26:56 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 let userName = prompt("Please enter your name:");
-const password = encodeURIComponent(sessionStorage.getItem(userName));
+const password = localStorage.getItem(userName);
 if(password === null) {
   let p_word = prompt("Password:");
   const expires = new Date(Date.now() + 60 * 60 * 10000); //365 * 24 * 60 * 60 * 1000 set the expiration date for the account to 1 year
