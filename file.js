3c82bd8369e380c69f2ec71c6842a4e1a0edd645
paddingtonowner <dianapatrice@gmail.com>
2024-12-13 17:15:20 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,2 +1,12 @@
 let userName = prompt("Please enter your name:");
-const username = sessionStorage.getItem(userName);
+const password = sessionStorage.getItem(userName);
+if(password is null) {
+  let p_word = prompt("Password:");
+  const expires = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000); // set the expiration date for the account to 1 year
+  document.cookie = `userName=p_word; expires=${expires.toUTCString()}; path=/`;
+} else {
+  let p_word = prompt("Password: ");
+  if(p_word === password) {
+    document.write("Logged In!")
+  }
+}
