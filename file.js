8bcaf94647759c1eba4bbf16ff047747e21c64ae
paddingtonowner <dianapatrice@gmail.com>
2024-12-13 17:25:53 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 let userName = prompt("Please enter your name:");
 const password = sessionStorage.getItem(userName);
-if(password is null) {
+if(password === null) {
   let p_word = prompt("Password:");
   const expires = new Date(Date.now() + 60 * 60 * 10000); //365 * 24 * 60 * 60 * 1000 set the expiration date for the account to 1 year
   document.cookie = `userName=p_word; expires=${expires.toUTCString()}; path=/`;
