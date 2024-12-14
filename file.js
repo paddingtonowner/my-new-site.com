bead911fed06b4e4666ad1bac319ef5b0abaa0c6
paddingtonowner <dianapatrice@gmail.com>
2024-12-14 12:47:48 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,11 +1,1 @@
-let userName = prompt("Please enter your name:");
-const password = localStorage.getItem(userName);
-if(password === null) {
-  let p_word = prompt("Password:");
-  localStorage.setItem(userName,p_word);
-} else {
-  let p_word = prompt("Password: ");
-  if(p_word === password) {
-    document.write("Logged In!")
-  }
-}
+console.log("Connected!");
